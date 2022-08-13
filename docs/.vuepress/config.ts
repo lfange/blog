03fbda0683a9aaeaf10d97e27923460a3cd1d187
FanGe <653398363@qq.com>
2022-08-13 20:20:51 +0800

    next

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,13 @@
+module.exports = {
+  lang: 'zh-CN',
+  title: 'Lfange Blog！',
+  description: '这是我的第一个 VuePress 站点',
+
+  locales: {
+    Locale: [
+      { text: "Home", link: "/" },
+      { text: "Guide", link: "/guide/" },
+      { text: "vue3", link: "/vue3/" },
+    ]
+  }
+}
